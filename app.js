3140bde8b3c9c8a538441309b3956b1a0d08ec2c
Austin Sprague <Sprague@Austins-MacBook-Pro.local>
2015-10-06 16:14:23 -0700

    changed objects to constructor

--- app.js
+++ app.js
@@ -1,157 +1,52 @@
 
-var hoursOfOps = ["10:00 AM","11:00 AM","12:00 PM","01:00 PM","02:00 PM","03:00 PM","04:00 PM","05:00 PM"];
+var hoursOfOps = ["10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
 
-var pikePlace={
-  location: 'Pike Place',
-  minCustHour: 17,
-  maxCustHour: 88,
-  avgCookiesPurch: 5.2,
-  randCustHour: function (){
+var CookieStand = function (location, minCustHour, maxCustHour, avgCookiesPurch, iD) {
+  this.place = location; 
+  this.minCustHour = minCustHour; 
+  this.maxCustHour = maxCustHour; 
+  this.avgCookiesPurch = avgCookiesPurch; 
+  this.totalCookiesList = []; 
+  this.iD = iD;
+
+  this.randCustHour = function (){
     return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour + 1) + this.minCustHour);
-  },
-  totalCookiesHour: function (){
-    return Math.floor(this.randCustHour() * this.avgCookiesPurch);
-  },
-  totalCookiesDay: function (){
-    return totalCookiesHour()*randCustHour();
+  };
+  
+  this.totalCookiesHour = function (){
+    for (var i=0; i < hoursOfOps.length; i++){
+      this.totalCookiesList.push(Math.floor(this.randCustHour() * this.avgCookiesPurch));
+      console.log("At " + hoursOfOps[i] + " " + this.place + " will need " + this.totalCookiesList[i] + " cookies");
+    }
+  };
+    
+  this.totalCookiesDay = function (){
+    var total = 0;
+    for (var i=0; i < this.totalCookiesList.length; i++){
+      total = total + this.totalCookiesList[i];
+    }
+    return total;
+  }; 
+
+  function display(){
+    for (var i=0; i < this.totalCookiesList.length; i++){
+      var list = document.getElementById("item");
+      var item = document.createElement("li");
+      var total = document.getElementById("total");
+      item.appendChild(document.createTextNode(hoursOfOps[i] + ': ' + this.totalCookiesList[i] + " cookies"));
+      list.appendChild(list);
+      item.appendChild(total);
+    }
+    return list;
   }
+  this.totalCookiesHour();
+  this.totalCookiesDay();
 }; 
-function pike(){
-  var array = [];
-  for (var i=0; i<hoursOfOps.length;i++){
-    array.push(pikePlace.totalCookiesHour());
-  }
-  for (var i=0; i<array.length;i++){
-    var list = document.getElementById("pike");
-    var item = document.createElement("ul");
-    var total = document.getElementById("pikeTotal");
-    item.appendChild(document.createTextNode(hoursOfOps[i] + ': ' + array[i] + " cookies"));
-    list.appendChild(item);
-    item.appendChild(total);
-  }
-  return list;
-}
 
-pike();
+var pikePlace = new CookieStand ('Pike Place Market', 17, 88, 5.2,'pike');
+var seaTac = new CookieStand ('SeaTac Ariport', 6,44,1.2,'seatac');
+var southCenter = new CookieStand('Southcenter Mall', 11,38,1.9,'southcenter');
+var bellevueSquare = new CookieStand('Bellevue Square', 20,48,3.3,'bellevue');
+var alki = new CookieStand('Alki Beach',3,24,2.6,'alki');
 
 
-
-var seaTac={
-  location: 'SeaTac Airport',
-  minCustHour: 6,
-  maxCustHour: 44,
-  avgCookiesPurch: 1.2,
-  randCustHour: function (){
-    return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour + 1) + this.minCustHour);
-  },
-  totalCookiesHour: function (){
-    return Math.floor(this.randCustHour() * this.avgCookiesPurch);
-  }
-}; 
-function seaTacAirport(){
-  var array = [];
-  for (var i=0; i<hoursOfOps.length;i++){
-    array.push(seaTac.totalCookiesHour());
-  }
-  for (var i=0; i<array.length;i++){
-    var list = document.getElementById("seatac");
-    var item = document.createElement("ul");
-    var total = document.getElementById("seatacTotal");
-    item.appendChild(document.createTextNode(hoursOfOps[i] + ': ' + array[i] + " cookies"));
-    list.appendChild(item);
-    item.appendChild(total);
-  }
-  return list;
-}
-
-seaTacAirport();
-
-var southCenter={
-  location: 'Southcenter Mall',
-  minCustHour: 11,
-  maxCustHour: 38,
-  avgCookiesPurch: 1.9,
-  randCustHour: function (){
-    return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour + 1) + this.minCustHour);
-  },
-  totalCookiesHour: function (){
-    return Math.floor(this.randCustHour() * this.avgCookiesPurch);
-  }
-}; 
-function southCenterMall(){
-  var array = [];
-  for (var i=0; i<hoursOfOps.length;i++){
-    array.push(southCenter.totalCookiesHour());
-  }
-  for (var i=0; i<array.length;i++){
-    var list = document.getElementById("southcenter");
-    var item = document.createElement("ul");
-    var total = document.getElementById("southcenterTotal");
-    item.appendChild(document.createTextNode(hoursOfOps[i] + ': ' + array[i] + " cookies"));
-    list.appendChild(item);
-    item.appendChild(total);
-  }
-  return list;
-}
-
-southCenterMall();
-
-var bellevueSq={
-  location: 'Bellevue Square',
-  minCustHour: 20,
-  maxCustHour: 48,
-  avgCookiesPurch: 3.3,
-  randCustHour: function (){
-    return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour + 1) + this.minCustHour);
-  },
-  totalCookiesHour: function (){
-    return Math.floor(this.randCustHour() * this.avgCookiesPurch);
-  }
-}; 
-function bellevueSquare(){
-  var array = [];
-  for (var i=0; i<hoursOfOps.length;i++){
-    array.push(southCenter.totalCookiesHour());
-  }
-  for (var i=0; i<array.length;i++){
-    var list = document.getElementById("bellevue square");
-    var item = document.createElement("ul");
-    var total = document.getElementById("bellevueTotal");
-    item.appendChild(document.createTextNode(hoursOfOps[i] + ': ' + array[i] + " cookies"));
-    list.appendChild(item);
-    item.appendChild(total);
-  }
-  return list;
-}
-
-bellevueSquare();
-
-var alki={
-  location: 'Alki Beach',
-  minCustHour: 3,
-  maxCustHour: 24,
-  avgCookiesPurch: 2.6,
-  randCustHour: function (){
-    return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour + 1) + this.minCustHour);
-  },
-  totalCookiesHour: function (){
-    return Math.floor(this.randCustHour() * this.avgCookiesPurch);
-  }
-}; 
-function alkiBeach(){
-  var array = [];
-  for (var i=0; i<hoursOfOps.length;i++){
-    array.push(alki.totalCookiesHour());
-  }
-  for (var i=0; i<array.length;i++){
-    var list = document.getElementById("alki");
-    var item = document.createElement("ul");
-    var total = document.getElementById("alkiTotal");
-    item.appendChild(document.createTextNode(hoursOfOps[i] + ': ' + array[i] + " cookies"));
-    list.appendChild(item);
-    item.appendChild(total);
-  }
-  return list;
-}
-
-alkiBeach();
